9d297669ab71eb2d70669db29d8e95751dc82c27
RFaska <93133836+playerRC@users.noreply.github.com>
2022-04-26 19:30:18 +0200

    codage du texte

--- Codage.py
+++ Codage.py
@@ -1,12 +1,17 @@
 import BinaryTree as bt
 
-# Etape 3
+# Etape 3: Codage du texte
 
+'''
+On créé une classe Codage avec un paramètre de type BinaryTree et un paramètre de type dictionaire qui va stocker le codage utilisé pour chaque caractère 
+et ce paramètre va nous servir ensuite à coder tout le texte par concaténation des codes de chacun de ses caractères
+'''
 class Codage():
     def __init__(self, arbre):
         self.arbre = arbre
         self.codage = {}
 
+    #on effectue un parcours en profondeur de notre arbre en mettant toujours le noeud racine comme paramètre de cette fonction
     def dic_codage_caractere(self, node, code_actuel = ""):
         if node == None:
             return
@@ -26,6 +31,3 @@ class Codage():
         file.close()
         return ch
 
-'''
-https://www.youtube.com/watch?v=JCOph23TQTY
-'''
